2a345f3a5f4c32805df4ef815bd6a34496c8ef4a
Denys G. Santos <gsdenys@gmail.com>
2017-04-19 21:28:29 -0300

    remove unused files and modify DockerFile

--- assets/install_ephesoft.sh
+++ assets/install_ephesoft.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-#EPHESOFT_VERSION=4.0.2.0
-
-#EPHESOFT_ZIP=Ephesoft_Community_Release_$EPHESOFT_VERSION.zip
-#EPHESOFT_FOLDER_BASE=Ephesoft_Community_$EPHESOFT_VERSION/
-#EPHESOFT_URL=http://www.ephesoft.com/Ephesoft_Product/$EPHESOFT_FOLDER_BASE/$EPHESOFT_ZIP
-
-
-# get Ephesoft zip installer
-#echo "Start do download Ephesoft version " $EPHESOFT_VERSION
-#mkdir -p $EPHESOFT_HOME
-#cd /tmp
-#curl -O $EPHESOFT_URL
-
 # install Ephesoft
 echo "Install Ephesoft in silentinstall mode..."
 cd /tmp
