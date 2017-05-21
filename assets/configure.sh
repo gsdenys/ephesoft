98cad8b62a3fde3d16df9a02291dff28b7b24d45
Denys G. Santos <gsdenys@gmail.com>
2017-05-21 17:30:55 -0300

    remove issues

--- assets/configure.sh
+++ assets/configure.sh
@@ -30,4 +30,4 @@ sed -i "/input_new_database_instance=/ s/=y/=n/" Response-Files/config.propertie
 chmod 755 install install-helper
 
 cd /tmp/
-wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/monkeyiq:/centos6updates/CentOS_CentOS-6/noarch/autoconf-2.69-12.2.noarch.rpm /tmp/
+wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/monkeyiq:/centos6updates/CentOS_CentOS-6/noarch/autoconf-2.69-12.2.noarch.rpm
